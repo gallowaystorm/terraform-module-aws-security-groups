48f556d57af025a653002c495ced2ba1b58ba2b0
Storm Galloway <storm.galloway@variq.com>
2021-03-09 08:24:32 -0500

    terraform fmt

--- security-group.tf
+++ security-group.tf
@@ -10,19 +10,19 @@ resource "aws_security_group" "security_group" {
     for_each = var.ingress_ports
     content {
       description = ingress.value.description
-      from_port = ingress.value.from_port
-      to_port   = ingress.value.to_port
-      protocol  = protocal.value.protocol
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = protocal.value.protocol
       cidr_blocks = ingress.value.cidr_blocks
     }
   }
-    dynamic "egress" {
+  dynamic "egress" {
     for_each = var.egress_ports
     content {
       description = egress.value.description
-      from_port = egress.value.from_port
-      to_port   = egress.value.to_port
-      protocol  = egress.value.protocol
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
       cidr_blocks = egress.value.cidr_blocks
     }
   }
