46c20a04df8fc9daac24084b5721ffca9cd10434
Storm Galloway <sgalloway@singledigits.com>
2021-03-10 15:52:23 -0500

    explanation of how to use

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,6 @@
+# To test out repo being called as module, change directory to "examples"
+# and then run terraform init/validate/apply
+# You will have to change the vpc_id in the YAML file and possibly the region below
 module "test-security-group" {
   source              = "../"
   security_group_list = yamldecode(file("${path.module}/security-groups.yaml"))
